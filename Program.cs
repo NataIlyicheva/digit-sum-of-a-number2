d566a62ebd2e2037ab1713bffdbe956e7ad9afcf
Natali <n7483205@ya.ru>
2022-10-19 20:03:53 +0700

    digit sum of a number /10

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int summ = 0;
+int number1 = 0;
+while (number > 0)
+{
+    number1 = number % 10;
+    number = number / 10;
+    summ = summ + number1;
+}
+Console.WriteLine($"итого {summ}");
